a343328dfeca57ccb2ea330378ecd697b3d219a4
konsti <kuttenkeulerbe@gmail.com>
2025-12-10 22:37:53 +0000

    Fix Entry Point

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App"; // Hier holt er sich deine App.jsx
+import "./index.css"; // Falls du styles hast, sonst wird es ignoriert
 
-const rootElement = document.getElementById("root")!;
+const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
